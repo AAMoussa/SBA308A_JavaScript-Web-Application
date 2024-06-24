a555bfc3e46956eeaa194025e199809c836b4d00
Ahmed Moussa <a.moussa0013@outlook.com>
2024-06-24 19:04:58 -0400

    Commiting reworked the Fetch API request files

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
-const fetchCats = require ('./modules/apiService.js');
-const displayCats = require ('./modules/ui.js') ;
+ async function getNewCat() {
 
-//add the event listener at loading
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "x-api-key": 'live_AQb1tl9xtDriPZcM3xEEKDtcquKdSBkw76NAPIBp34fpNThRqEHGkaRClEmIM8nY'
+    });
+    
+    var requestOptions = {
+        method: 'GET',
+        headers: headers,
+        redirect: 'follow'
+    };
+    
+    fetch("https://api.thecatapi.com/v1/images/search?limit=20", requestOptions)
+    .then(response => response.json())
+    .then(result => displayCats(result))
+    .catch(error => console.log('error', error));
+         
+ }
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchCats()
-        .then(cats => { displayCats(cats);})
-        .catch(error => { 
-            console.error('Error Retrieving cats: ', error);
+    function displayCats(cats) {
+        const catGallery = document.getElementById('catGallery');
+        catGallery.innerHTML = '';
+        cats.forEach(cat => {
+            const catElement = document.createElement('img');
+            catElement.classList.add('cat-image');
+            catElement.id = cat.id;
+            catElement.height = cat.height;
+            catElement.width = cat.width;
+            catElement.src = cat.url;
+            catElement.alt = 'A cute cat';
+            catGallery.appendChild(catElement);
         });
-});
+    }
 
+    getNewCat();
